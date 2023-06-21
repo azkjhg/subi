8b4dd6999a70dd5d8ec357bb6e663f4798ce894b
bumrae <azkjhg@naver.com>
2023-06-21 21:00:25 +0900

    color

--- components/Card.js
+++ components/Card.js
@@ -1,7 +1,10 @@
+"use client";
 import Image from "next/image";
-import React from "react";
 import Link from "next/link";
+import { MyContext } from './Context';
+import React, { useContext } from "react";
 const Card = ({ result, list }) => {
+  const cont = useContext(MyContext);
   const DataDelete = (e, i) => {
     if (result.url && result.imageRef) {
       const imageRef = ref(storage, result.imageRef._location.path_);
@@ -33,9 +36,172 @@ const Card = ({ result, list }) => {
         }
       });
   };
-  return (
+  const buttonClick = (어떤거) => {
+    let 카테고리 = []
+    if(어떤거 == '전체'){
+      카테고리 = []
+      result.forEach(item => {
+        // cont.value = [null, ...cont.value.slice(1)];
+        // 매물정보 객체의 '매물정보' 속성이 '아파트'인 경우 result에 추가
+        if (true) {
+          // setNewResult(prevResult => [...prevResult, item]);
+          카테고리 = [...카테고리, item]
+        }
+      });
+      cont.setValue([카테고리, '전체'])
+    }
+else if(어떤거 == '아파트'){
+  카테고리 = []
+  result.forEach(item => {
+    // 매물정보 객체의 '매물정보' 속성이 '아파트'인 경우 result에 추가
+    // cont.value = [null, ...cont.value.slice(1)];
+    if (item.매물종류 == '아파트') {
+      // setNewResult(prevResult => [...prevResult, item]);
+      카테고리 = [...카테고리, item]
+      
+    }
+    cont.setValue([카테고리, '아파트'])
+  });
+}
+else if(어떤거 == '상가건물'){
+  카테고리 = []
+  result.forEach(item => {
+    // 매물정보 객체의 '매물정보' 속성이 '아파트'인 경우 result에 추가
+    // cont.value = [null, ...cont.value.slice(1)];
+    if (item.매물종류 == '상가건물') {
+      // setNewResult(prevResult => [...prevResult, item]);
+      카테고리 = [...카테고리, item]
+    }
+  });
+  cont.setValue([카테고리, '상가건물'])
+}
+
+else if(어떤거 == '상가점포'){
+  카테고리 = []
+  result.forEach(item => {
+    // cont.value = [null, ...cont.value.slice(1)];
+    // 매물정보 객체의 '매물정보' 속성이 '아파트'인 경우 result에 추가
+    if (item.매물종류 == '상가점포') {
+      // setNewResult(prevResult => [...prevResult, item]);
+      카테고리 = [...카테고리, item]
+    }
+  });
+  cont.setValue([카테고리, '상가점포'])
+}
+else if(어떤거 == '상가주택'){
+  카테고리 = []
+  result.forEach(item => {
+    // cont.value = [null, ...cont.value.slice(1)];
+    // 매물정보 객체의 '매물정보' 속성이 '아파트'인 경우 result에 추가
+    if (item.매물종류 == '상가주택') {
+      // setNewResult(prevResult => [...prevResult, item]);
+      카테고리 = [...카테고리, item]
+    }
+  });
+  cont.setValue([카테고리, '상가주택'])
+}
+else if(어떤거 == '빌라'){
+  카테고리 = []
+  result.forEach(item => {
+    // cont.value = [null, ...cont.value.slice(1)];
+    // 매물정보 객체의 '매물정보' 속성이 '아파트'인 경우 result에 추가
+    if (item.매물종류 == '빌라') {
+      // setNewResult(prevResult => [...prevResult, item]);
+      카테고리 = [...카테고리, item]
+    }
+  });
+  cont.setValue([카테고리, '빌라'])
+}
+else if(어떤거 == '단독'){
+  카테고리 = []
+  result.forEach(item => {
+    // cont.value = [null, ...cont.value.slice(1)];
+    // 매물정보 객체의 '매물정보' 속성이 '아파트'인 경우 result에 추가
+    if (item.매물종류 == '단독') {
+      // setNewResult(prevResult => [...prevResult, item]);
+      카테고리 = [...카테고리, item]
+    }
+  });
+  cont.setValue([카테고리, '단독'])
+}
+else if(어떤거 == '토지'){
+  카테고리 = []
+  result.forEach(item => {
+    // cont.value = [null, ...cont.value.slice(1)];
+    // 매물정보 객체의 '매물정보' 속성이 '아파트'인 경우 result에 추가
+    if (item.매물종류 == '토지') {
+      // setNewResult(prevResult => [...prevResult, item]);
+      카테고리 = [...카테고리, item]
+    }
+  });
+  cont.setValue([카테고리, '토지'])
+}
+    // result 초기화
+    // cont.setValue([]);
+    // 매물정보 배열 순회
+    return
+  };
+console.log(cont.value[0])
+  let 전체;
+  if(cont.value[1] =="전체"){
+    전체 = "text-black bg-white border-2 border-gray-500 hover:bg-blue-800 hover:text-white focus:ring-4 focus:ring-blue-300 focus:text-black focus:bg-white font-bold rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 box-border"
+  } else{
+    전체 = "text-white bg-blue-700 border-2 border-blue-300 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 focus:text-black focus:bg-white font-bold rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+  }
+  let 아파트;
+  if(cont.value[1] =="아파트"){
+    아파트 = "text-black bg-white border-2 border-gray-500 hover:bg-blue-800 hover:text-white focus:ring-4 focus:ring-blue-300 font-bold focus:text-black focus:bg-white rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 box-border"
+  } else{
+    아파트 = "text-white bg-blue-700 border-2 border-blue-300 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-bold focus:text-black focus:bg-white rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+  }
+  let 상가건물;
+  if(cont.value[1] =="상가건물"){
+    상가건물 = "text-black bg-white border-2 border-gray-500 hover:bg-red-800 hover:text-white focus:ring-4 focus:ring-red-300 font-bold focus:text-black focus:bg-white rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 box-border"
+  } else{
+    상가건물 = "text-white bg-red-700 border-2 border-red-300 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-bold focus:text-black focus:bg-white rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+  }
+  let 상가주택;
+  if(cont.value[1] =="상가주택"){
+    상가주택 = "text-black bg-white border-2 border-gray-500 hover:bg-red-800 hover:text-white focus:ring-4 focus:ring-red-300 font-bold focus:text-black focus:bg-white rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 box-border"
+  } else{
+    상가주택 = "text-white bg-red-700 border-2 border-red-300 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-bold focus:text-black focus:bg-white rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+  }
+  let 상가점포;
+  if(cont.value[1] =="상가점포"){
+    상가점포 = "text-black bg-white border-2 border-gray-500 hover:bg-red-800 hover:text-white focus:ring-4 focus:ring-red-300 font-bold focus:text-black focus:bg-white rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 box-border"
+  } else{
+    상가점포 = "text-white bg-red-700 border-2 border-red-300 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-bold focus:text-black focus:bg-white rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+  }
+  let 빌라;
+  if(cont.value[1] =="빌라"){
+    빌라 = "text-black bg-white border-2 border-gray-500 hover:bg-indigo-800 hover:text-white focus:ring-4 focus:ring-indigo-300 font-bold focus:text-black focus:bg-white rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 box-border"
+  } else{
+    빌라 = "text-white bg-indigo-700 border-2 border-indigo-300 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 font-bold focus:text-black focus:bg-white rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+  }
+  let 단독;
+  if(cont.value[1] =="단독"){
+    단독 = "text-black bg-white border-2 border-gray-500 hover:bg-purple-800 hover:text-white focus:ring-4 focus:ring-purple-300 font-bold focus:text-black focus:bg-white rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 box-border"
+  } else{
+    단독 = "text-white bg-purple-700 border-2 border-purple-300 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-bold focus:text-black focus:bg-white rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+  }
+  let 토지;
+  if(cont.value[1] =="토지"){
+    토지 = "text-black bg-white border-2 border-gray-500 hover:bg-green-800 hover:text-white focus:ring-4 focus:ring-green-300 font-bold focus:text-black focus:bg-white rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 box-border"
+  } else{
+    토지 = "text-white bg-green-700 border-2 border-green-300 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-bold focus:text-black focus:bg-white rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+  }
+
+  return (<>
+  <button type="button" className={전체} onClick={() => buttonClick('전체')}>전체</button>
+        <button type="button" className={아파트} onClick={() => buttonClick('아파트')}>아파트</button>
+        <button type="button" className={상가건물} onClick={() => buttonClick('상가건물')}>상가건물</button>
+        <button type="button" className={상가주택} onClick={() => buttonClick('상가주택')}>상가주택</button>
+        <button type="button" className={상가점포} onClick={() => buttonClick('상가점포')}>상가점포</button>
+        <button type="button" className={빌라} onClick={() => buttonClick('빌라')}>빌라/다세대</button>
+        <button type="button" className={단독} onClick={() => buttonClick('단독')}>단독/다가구</button>
+        <button type="button" className={토지} onClick={() => buttonClick('토지')}>토지/임야</button>
     <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-1">
-      {result.map((item, i) => {
+      {(cont.value[0] || result).map((item, i) => {
 const formatAmount = (value) => {
   const parts = value.split('');
   let formattedValue = '';
@@ -313,6 +479,7 @@ const formatAmount = (value) => {
         );
       })}
     </div>
+    </>
   );
 };
 
